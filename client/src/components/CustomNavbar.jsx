ec905f4f1ede07bcc62aa1a9324e644c4ded33ab
shardul koli <monty@sharduls-MBP.fios-router.home>
2022-04-26 14:14:10 -0400

    React base templates for pages

---
+++ client/src/components/CustomNavbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from "./CustomNavbar.module.css";
+import FilterListIcon from "@mui/icons-material/FilterList";
+
+export const CustomNavbar = () => {
+  return (
+    <div>
+      <Navbar bg="dark" expand="lg">
+        <Container>
+          <Navbar.Brand>
+            <img
+              src={require("../assets/logo192.png")}
+              width="40"
+              height="40"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+
+          {/* <Navbar.Collapse id="basic-navbar-nav"> */}
+          <Nav>
+            <div className={styles.linkContainer}>
+              <Link to="/" className={styles.linkchange}>
+                Home
+              </Link>
+            </div>
+            {/* <div className={styles.linkContainer}>
+              <Link to="/login" className={styles.linkchange}>
+                Login
+              </Link>
+            </div> */}
+            <div className={styles.linkContainer}>
+              <FilterListIcon sx={{ color: "white" }}></FilterListIcon>
+              <Link to="/account/1" className={styles.linkchange}>
+                Account
+              </Link>
+            </div>
+          </Nav>
+          {/* </Navbar.Collapse> */}
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
