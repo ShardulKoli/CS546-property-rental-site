fbc4ba30035e056086e96ae35e2f79a11c5192b7
Aishwarya Pai <apai4@stevens.edu>
2022-04-28 16:37:39 -0400

    validation/validations.js

--- validation/validations.js
+++ validation/validations.js
@@ -1,3 +1,5 @@
+const { Double } = require("mongodb");
+
 module.exports = {
     checkId(id) {
       console.log("TYPE 1 OF ID :: "+typeof id);
@@ -81,7 +83,7 @@ module.exports = {
         }
         console.log("TYPE 2 OF contact :: "+typeof contact);
     },
-    validatePassword(){
+    validatePassword(password){
         if (!password)
             throw "You must provide a Password";
         if (typeof password !== 'string')
@@ -90,6 +92,118 @@ module.exports = {
             throw "Password cannot be an empty string or string with just spaces";
         if (password.trim().length < 6)
             throw "Password should be atleast 6 characters long.";
-    }
-
+    },
+    validatePincode(pincode){
+        pincode = Number(pincode);
+        if (Number.isInteger(pincode) && pincode > 0) {
+            if (!pincode || pincode === "" || pincode === undefined) throw 'Error: You must provide Pincode';
+            if (pincode.length === 0)
+            throw 'Error: Pincode cannot be an empty string or just spaces';
+            return pincode;
+        }else{
+            throw 'Error: Pincode must be a positive whole number';
+        }
+    },
+    validateCity(city){
+        if (!city)
+            throw "You must provide a City";
+        if (typeof city !== 'string')
+            throw "City must be a string";
+        if (city.trim().length === 0)
+            throw "City cannot be an empty string or string with just spaces";
+    },
+    validateState(state){
+        if (!state)
+            throw "You must provide a State";
+        if (typeof state !== 'string')
+            throw "State must be a string";
+        if (state.trim().length === 0)
+            throw "State cannot be an empty string or string with just spaces";
+    },
+    validatePropertyType(propType){
+        if (!propType)
+            throw "You must provide a Property Type";
+        if (typeof propType !== 'string')
+            throw "Property Type must be a string";
+        if (propType.trim().length === 0)
+            throw "Property Type cannot be an empty string or string with just spaces";
+    },
+    validateBeds(beds){
+        beds = Number(beds);
+        if (Number.isInteger(beds) && beds > 0) {
+            if (!beds || beds === "" || beds === undefined) throw 'Error: You must provide number of beds';
+            if (beds.length === 0)
+            throw 'Error: Beds cannot be an empty string or just spaces';
+            return beds;
+        }else{
+            throw 'Error: Beds must be a positive whole number';
+        }
+    },
+    validateBaths(baths){
+        baths = Number(baths);
+        if (Number.isInteger(baths) && baths > 0) {
+            if (!baths || baths === "" || baths === undefined) throw 'Error: You must provide number of baths';
+            if (baths.length === 0)
+            throw 'Error: Number of baths cannot be an empty string or just spaces';
+            return baths;
+        }else{
+            throw 'Error: Number of baths must be a positive whole number';
+        }
+    },
+    validateBalcony(balcony){
+        balcony = Number(balcony);
+        if (Number.isInteger(balcony) && balcony > 0) {
+            if (!balcony || balcony === "" || balcony === undefined) throw 'Error: You must provide number of baths';
+            if (balcony.length === 0)
+            throw 'Error: Number of balcony cannot be an empty string or just spaces';
+            return balcony;
+        }else{
+            throw 'Error: Number of balcony must be a positive whole number';
+        }
+    },
+    validateRent(rent){
+        rent = Number(rent);
+        if (Number.isInteger(rent) && rent > 0) {
+            if (!rent || rent === "" || rent === undefined) throw 'Error: You must provide rent';
+            if (rent.length === 0)
+            throw 'Error: Rent cannot be an empty string or just spaces';
+            return rent;
+        }else{
+            throw 'Error: Rent must be a positive whole number';
+        }
+    },
+    validateBrokerage(brokerage){
+        brokerage = Double(brokerage);
+        if (!brokerage || brokerage === "" || brokerage === undefined) throw 'Error: You must provide Brokerage';
+        if (brokerage.length === 0)
+        throw 'Error: Brokerage cannot be an empty string or just spaces';
+        return brokerage;
+    },
+    validateDeposit(deposit){
+        deposit = Double(deposit);
+        if (!deposit || deposit === "" || deposit === undefined) throw 'Error: You must provide Deposit';
+        if (deposit.length === 0)
+        throw 'Error: Deposit cannot be an empty string or just spaces';
+        return deposit;
+    },
+    validateCentralAir(centralAir){
+        if (typeof centralAir != "boolean") {
+            throw 'Error: Central air must be of a boolean type';
+        }
+    },
+    validatePetFriendly(petFriendly){
+        if (typeof petFriendly != "boolean") {
+            throw 'Error: Pet Friendly must be of a boolean type';
+        }
+    },
+    validatePartyFriendly(partyFriendly){
+        if (typeof partyFriendly != "boolean") {
+            throw 'Error: Party Friendly must be of a boolean type';
+        }
+    },
+    validateGarrage(garrage){
+        if (typeof garrage != "boolean") {
+            throw 'Error: Garrage must be of a boolean type';
+        }
+    },
 };
