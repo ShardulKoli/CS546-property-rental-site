9fa4764e8e157b7571130e3ad5718781e139ad7e
EZ-Shaunak <shaunak4kulkarni@gmail.com>
2022-05-05 21:22:11 -0400

    admin panel get user and prop list

--- public/adminscript.js
+++ public/adminscript.js
@@ -13,7 +13,6 @@ function verifyPassword() {
                     $("#mainDiv").show();
 
 
-
                 } else {
                     $("#errorPassDiv").show();
                 }
@@ -37,11 +36,87 @@ function logout(e) {
     $("#detailsDiv").html('');
     $("#detailsDiv").hide('');
     $("#txtPass").val('');
+    $("#dataListUl").html('');
+    $("#listDiv").hide();
 
 }
 
 function getUsers(e, type) {
     e.preventDefault();
 
-    
+    $.ajax({
+        type: "GET",
+        url: "/admin/getUsers/" + type,
+        success: function (data) {
+            if (data.status) {
+
+                $("#passDiv").hide();
+
+                var userListHtml = getUserListHtml(data.users);
+
+                $("#dataListUl").html(userListHtml);
+                $("#listDiv").show();
+
+            } else {
+
+            }
+        },
+        error(xhr, status, error) {
+            alert(error);
+        }
+    });
+}
+
+function getProperties(e, type) {
+    e.preventDefault();
+
+    $.ajax({
+        type: "GET",
+        url: "/admin/getProperties/",
+        success: function (data) {
+            if (data.status) {
+
+                $("#passDiv").hide();
+
+                var userListHtml = getPropertyListHtml(data.properties);
+
+                $("#dataListUl").html(userListHtml);
+                $("#listDiv").show();
+
+            } else {
+
+            }
+        },
+        error(xhr, status, error) {
+            alert(error);
+        }
+    });
+}
+
+function getUserListHtml(users) {
+
+    var html = '';
+
+    $.each(users, function (i) {
+        var li = '<li><a href="' + this._id + '" id="' + this._id + '" onclick="getUserDetail(event,this)">'
+            + this.firstName + ' ' + this.lastName + '-' + this.email + '</a></li>';
+
+        html += li;
+    });
+
+    return html;
+
+}
+
+function getPropertyListHtml(props) {
+    var html = '';
+
+    $.each(props, function (i) {
+        var li = '<li><a href="' + this._id + '" id="' + this._id + '" onclick="getPropertyDetail(event,this)">'
+            + this.name + '-' + this.address + '-' + this.pincode + '-' + this.city + '-' + this.state + '</a></li>';
+
+        html += li;
+    });
+
+    return html;
 }
